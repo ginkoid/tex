c18e7a31cdb1f163d62a6cc2a67bb593e3048907
Philip Papurt <ginkoid@gmail.com>
2021-01-19 03:18:28 -0500

    fix: limit tmpfs size

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /sys/fs/cgroup/{memory,cpu}/NSJAIL
+mkdir /sys/fs/cgroup/{memory,cpu}/NSJAIL
 chown nsjail /sys/fs/cgroup/{memory,cpu}/NSJAIL
 
-exec setpriv --init-groups --reuid nsjail --regid nsjail --inh-caps=-all --bounding-set=-all /app/nsjail -C /app/nsjail.cfg
+exec setpriv --init-groups --reuid nsjail --regid nsjail --ambient-caps=-all --inh-caps=-all --bounding-set=-all /app/nsjail -C /app/nsjail.cfg
